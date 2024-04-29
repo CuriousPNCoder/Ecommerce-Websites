5e53fa8b4bd6640e5bc8ef88d73d7750c0d9c9c8
CuriousPNCoder <paramitanandi65@gmail.com>
2024-04-29 16:30:54 +0530

    update

--- src/components/styles/herosection.js
+++ src/components/styles/herosection.js
@@ -43,12 +43,12 @@ const HeroSection = () => {
             <button
               type="button"
               className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-3 text-center me-2 mb-2">
-              Purple to Blue
+            Buy lattest product
             </button>
 
             <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-blue-500 rounded-lg border-gradient-to-br border-2 border-purple-600 group group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
               <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-transparent  text-purple-600 dark:bg-transparent rounded-md group-hover:bg-opacity-0">
-                Blue Text
+               Know about website
               </span>
             </button>
           </div>
